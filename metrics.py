92afd3750dc2d927f852b54c7b3b7d0ef84998a4
arina-chumachenko <92745624+arina-chumachenko@users.noreply.github.com>
2025-07-24 15:07:27 +0800

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -5,6 +5,10 @@ import sys
 import nltk
 import numpy as np
 from nltk.translate.meteor_score import meteor_score
+from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
+from nltk.tokenize import word_tokenize
+from nltk.translate.bleu_score import SmoothingFunction
+from rouge import Rouge
 
 nltk.download('wordnet', quiet=True)
 
@@ -20,14 +24,56 @@ def main():
     with open(args.input_json, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    scores = []
+    meteor_scores = []
+    bleu1_scores, bleu2_scores, bleu4_scores, corpus_bleu_scores = [], [], [], []
+    rouge1_scores, rouge2_scores, rougel_scores = [], [], []
+
+    smoother = SmoothingFunction().method1
+
     for sample in data:
-        true_ans = [sample["response"].split()]
-        pred_ans = sample["pred_response"].split()
+        true_ans = sample["response"]
+        pred_ans = sample["pred_response"]
 
-        scores.append(meteor_score(true_ans, pred_ans))
+        splited_true_ans = [true_ans.split()]
+        splited_pred_ans = pred_ans.split()
+    
+        # METEOR
+        meteor_scores.append(meteor_score(splited_true_ans, splited_pred_ans))
+   
+        rouge = Rouge()
+        rouge_score = rouge.get_scores(true_ans, pred_ans, avg=True)
 
-    print(f"Meteor score: {np.mean(scores)}")
+        # BLEU 
+        bleu1_scores.append(sentence_bleu(
+            splited_true_ans, splited_pred_ans, 
+            weights=(1, 0, 0, 0), 
+            smoothing_function=smoother,
+        ))
+        bleu2_scores.append(sentence_bleu(
+            splited_true_ans, splited_pred_ans, 
+            weights=(0.5, 0.5, 0, 0),
+            smoothing_function=smoother
+        ))
+        bleu4_scores.append(sentence_bleu(
+            splited_true_ans, splited_pred_ans, 
+            weights=(0.25, 0.25, 0.25, 0.25),
+            smoothing_function=smoother,
+        ))
+
+        # Rouge
+        rouge1_scores.append(rouge_score['rouge-1']['f'])
+        rouge2_scores.append(rouge_score['rouge-1']['f'])
+        rougel_scores.append(rouge_score['rouge-1']['f'])
+
+    return {
+        "meteor": np.mean(meteor_scores),
+        "bleu1": np.mean(bleu1_scores),
+        "bleu2": np.mean(bleu2_scores),
+        "bleu4": np.mean(bleu4_scores),
+        "rouge1": np.mean(rouge1_scores),
+        "rouge2": np.mean(rouge2_scores),
+        "rougel": np.mean(rougel_scores),
+    }
 
 
 if __name__ == "__main__":
